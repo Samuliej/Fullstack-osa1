050623e267523d5a5720a0fa3a90d21e516c50a2
Samuli Toppi <samuli.e.j.toppi@student.jyu.fi>
2022-06-01 12:52:32 +0300

    Fullstack-osa1 teht1 step3

--- teht1.2/src/App.js
+++ teht1.2/src/App.js
@@ -1,10 +1,12 @@
 const Header = (props) => {
+  console.log(props);
   return (
     <h1>{props.course}</h1>
   )
 }
 
 const Part = (props) => {
+  console.log(props);
   return (
     <p>
       {props.part}
@@ -13,6 +15,7 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props);
   return (
     <div>
       <Part part={props.part1} />
@@ -23,6 +26,7 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
+  console.log(props);
   return (
     <p>
       Number of exercises {props.total}
@@ -31,22 +35,27 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  // Lisäsin välilyönnit part osioiden loppuun
-  const part1 = 'Fundamentals of React '
-  const exercises1 = 10
-  const part2 = 'Using props to pass data '
-  const exercises2 = 7
-  const part3 = 'State of a component '
-  const exercises3 = 14
+  const course = 'Half Stack application development '
+  const part1 = {
+    name: 'Fundamentals of React ',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data ',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component ',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1 + exercises1} 
-               part2={part2 + exercises2}
-               part3={part3 + exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1.name + part1.exercises} 
+               part2={part2.name + part2.exercises}
+               part3={part3.name + part3.exercises} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
