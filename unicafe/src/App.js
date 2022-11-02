fcecb32e2f3f1fe2d028b1c7eb4e3ffe78bb2af2
Samuli Toppi <samuli.e.j.toppi@student.jyu.fi>
2022-11-02 14:59:58 +0200

    1.6: unicafe step-1 complete

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Button = (props) => (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+)
+
+const Header = (props) => (
+  <h1>
+    {props.text}
+  </h1>
+)
+
+const Display = (props) => (
+  <div>
+    <p>{props.text}: {props.value}</p>
+  </div>
+)
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setGoodValue = () => setGood(good +1)
+  const setNeutralValue = () => setNeutral(neutral +1)
+  const setBadValue = () => setBad(bad +1)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header text={"give feedback"}></Header>
+        <Button handleClick={() => setGoodValue()} text="good"></Button>
+        <Button handleClick={() => setNeutralValue()} text="neutral"></Button>
+        <Button handleClick={() => setBadValue()} text="bad"></Button>
+        <Header text={"statistics"}></Header>
+        <Display text="good" value={good}></Display>
+        <Display text="neutral" value={neutral}></Display>
+        <Display text="bad" value={bad}></Display>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
